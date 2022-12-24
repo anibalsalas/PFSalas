a4856450ae01980c88c383a0c75b419e000c3e9d
Anival Salas <66155820+anibalsalas@users.noreply.github.com>
2022-12-24 01:51:34 -0500

    agregando js

--- js/carrito.js
+++ js/carrito.js
@@ -3,7 +3,7 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaProductos = document.querySelector('#lista-productos');
-
+let articuloCarrito = []
 
 cargarEventListeners()
 function cargarEventListeners(){
@@ -31,8 +31,51 @@ function leerDatosProducto(producto){
     const infoProducto = {
         imagen: producto.querySelector('img').src,
         titulo: producto.querySelector('h5').textContent,
-        precio: producto.querySelector('h4').textContent
+        precio: producto.querySelector('h4').textContent,
+        id: producto.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
     }
-    console.log(infoProducto)
+    //agrega elementos al arreglo del carrito
+    articuloCarrito = [...articuloCarrito, infoProducto];
+    console.log(articuloCarrito);
+    carritoHTML();
+}
 
+//Muestra el carrito de compras en el HTML
+
+function carritoHTML(){
+
+    //Limpia HTML
+    limpiaHTML();
+    //Recorre el carrito y genera el HTML
+    articuloCarrito.forEach( producto =>{
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+            <img src="${producto.imagen}" width="100">
+        </td>
+        <td>
+            ${producto.titulo}
+         </td>
+        <td>
+            ${producto.precio}
+        </td>
+         <td>
+            ${producto.cantidad}
+        </td>
+        `;
+
+        //Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+
+    });
+
+    //Elimina los cursos del tbody
+    function limpiaHTML(){
+        //contenedorCarrito.innerHTML = '';
+
+        while(contenedorCarrito.firstChild){
+            contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+        }
+    }
 }
